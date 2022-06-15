6e9676f8f78a01588f0acd5c5060e1ae5743d6be
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-15 15:54:25 -0700

    Add method to configure stage in HelloController from HelloApplication

--- src/main/java/com/example/pdfmerge/HelloController.java
+++ src/main/java/com/example/pdfmerge/HelloController.java
@@ -1,18 +1,27 @@
 package com.example.pdfmerge;
 
+import com.example.pdfmerge.model.PDFMerger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 public class HelloController {
     public Button btnChooseFiles;
     public ScrollPane selectedFilesPane;
     public Button btnMerge;
+    private static Stage baseStage;
+
+    private PDFMerger merger = new PDFMerger();
+
+    public static void initStage(Stage stage) {
+        baseStage = stage;
+    }
 
     public void onChooseButtonClick(ActionEvent actionEvent) {
-
+        this.merger.openFileChooser(baseStage);
     }
 
     public void onMergeButtonClick(ActionEvent actionEvent) {
