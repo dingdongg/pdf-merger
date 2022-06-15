be5520d3cd85ba438836aa46813eb2d8c3bbe3c5
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-15 16:13:46 -0700

    Update selected files pane when files are chosen

--- src/main/java/com/example/pdfmerge/HelloController.java
+++ src/main/java/com/example/pdfmerge/HelloController.java
@@ -2,19 +2,20 @@ package com.example.pdfmerge;
 
 import com.example.pdfmerge.model.PDFMerger;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.List;
 
 public class HelloController {
     public Button btnChooseFiles;
-    public ScrollPane selectedFilesPane;
+    public ListView<String> selectedFilesPane = new ListView<>();
     public Button btnMerge;
     private static Stage baseStage;
 
-    private PDFMerger merger = new PDFMerger();
+    private final PDFMerger merger = new PDFMerger();
 
     public static void initStage(Stage stage) {
         baseStage = stage;
@@ -22,6 +23,14 @@ public class HelloController {
 
     public void onChooseButtonClick(ActionEvent actionEvent) {
         this.merger.openFileChooser(baseStage);
+        updateSelectedFilesPane();
+    }
+
+    private void updateSelectedFilesPane() {
+        List<File> files = this.merger.getPDFfiles();
+        for (File f : files) {
+            this.selectedFilesPane.getItems().add(f.getName());
+        }
     }
 
     public void onMergeButtonClick(ActionEvent actionEvent) {
