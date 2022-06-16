d0ebd391bf0370b661f3e34485f2d3bca528515a
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-15 23:08:35 -0700

    Change application name

--- src/main/java/com/example/pdfmerge/HelloApplication.java
+++ src/main/java/com/example/pdfmerge/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.pdfmerge;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("PDF Merger");
         stage.setScene(scene);
         HelloController.initStage(stage); // passes in this stage for use in HelloController
         stage.show();
