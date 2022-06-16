76e67ffb0a5fc8a441dcf33975f414cc6263992d
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-16 16:40:16 -0700

    Create class constructor

--- src/main/java/com/example/pdfmerge/exceptions/NoFilesException.java
+++ src/main/java/com/example/pdfmerge/exceptions/NoFilesException.java
@@ -2,4 +2,8 @@ package com.example.pdfmerge.exceptions;
 
 // exception thrown when no files are selected for merging
 public class NoFilesException extends Exception {
+
+    public NoFilesException(String msg) {
+        super(msg);
+    }
 }
