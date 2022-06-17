d1f2c0e842451802fe5ce92fdf893528fb7d369b
donggyu <82309121+dingdongg@users.noreply.github.com>
2022-06-16 17:31:22 -0700

    Scale size of application on boot based on screen resolution

--- src/main/java/com/example/pdfmerge/MergerApplication.java
+++ src/main/java/com/example/pdfmerge/MergerApplication.java
@@ -2,7 +2,9 @@ package com.example.pdfmerge;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +13,8 @@ public class MergerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MergerApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Rectangle2D screenRes = Screen.getPrimary().getBounds();
+        Scene scene = new Scene(fxmlLoader.load(), screenRes.getWidth() / 2, screenRes.getHeight() * 3 / 4);
         stage.setTitle("PDF Merger");
         stage.setScene(scene);
         MergerController.initStage(stage); // passes in this stage for use in HelloController
